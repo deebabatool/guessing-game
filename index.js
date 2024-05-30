33a4f2137db8cc136efa91718d734f297fc98a74
deebabatool <161479432+deebabatool@users.noreply.github.com>
2024-05-30 19:18:02 +0500

    this is a guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+//Let's create A Number_Guessing_Game
+import inquirer from "inquirer";
+const Guess_Random_Numb = Math.floor(Math.random() * 10 + 1);
+const Number_Guessing_Game = await inquirer.prompt([
+    {
+        name: "guess_numb",
+        type: "number",
+        message: "please guessed the number between 1-10 ?",
+    },
+]);
+if (Number_Guessing_Game.guess_numb == Guess_Random_Numb) {
+    console.log("Congrats your guessed is correct");
+}
+else {
+    console.log("Opzzz you guessed wrong!");
+}
